41532055fa5667201e2ddf327da6c95726adcd8f
Urs Kober <urs.kober@mailbox.tu-dresden.de>
2024-03-18 10:02:15 +0100

    add multi threading for main work

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
+#include <pthread.h>
+#include <thread>
+#include <unistd.h>
 #include <vector>
 
 #include "ecc/ecc.hpp"
@@ -100,110 +103,46 @@ std::array<uint16_t, 8> bit_position_enumeration_idx_burst(uint64_t n, uint64_t 
     return ret;
 }
 
-int main(int argc, char** argv)
-{
-    // the settings we will set with clas
-    enum FAIL_MODE {
-        FAIL_MODE_NONE = 0,
-        FAIL_MODE_RANDOM,
-        FAIL_MODE_RANDOM_BURST,
-    };
+struct ecc_stats {
+    uint64_t detection_ok = 0;
+    uint64_t detection_corrected = 0;
+    uint64_t detection_uncorrectable = 0;
+    uint64_t false_corrections = 0;
+};
 
-    bool full_run = false;
+enum FAIL_MODE {
+    FAIL_MODE_NONE = 0,
+    FAIL_MODE_RANDOM,
+    FAIL_MODE_RANDOM_BURST,
+};
+
+struct thread_control {
+    pthread_t pthread_id;
+    bool full_run;
     FAIL_MODE fail_mode;
     uint32_t fail_count;
-    uint64_t test_count;
+    uint64_t rng_seed;
     ECCMethod* method;
-    uint64_t seed = 42;
+    uint64_t work_offset;
+    uint64_t work_progress;
+    uint64_t work_max;
+    ecc_stats stats;
+};
 
-    // parse clas
-    if (argc < 6) {
-        errorf("usage: <fail_mode> <fail_count> <test_count> <ecc_method> <ecc_conf> [seed]\n");
-    }
+void* thread_work(void* arg)
+{
+    thread_control& ctrl = *(thread_control*)arg;
 
-    const char* arg_fail_mode = argv[1];
-    const char* arg_fail_count = argv[2];
-    const char* arg_test_count = argv[3];
-    const char* arg_ecc_method = argv[4];
-    const char* arg_ecc_conf = argv[5];
-    const char* arg_seed = argc > 6 ? argv[6] : NULL;
-    bool debug_print = argc > 7;
+    uint64_t rctr = 0;
 
-    if (strcmp(arg_fail_mode, "N") == 0) {
-        fail_mode = FAIL_MODE_NONE;
-    } else if (strcmp(arg_fail_mode, "R") == 0) {
-        fail_mode = FAIL_MODE_RANDOM;
-    } else if (strcmp(arg_fail_mode, "RB") == 0) {
-        fail_mode = FAIL_MODE_RANDOM_BURST;
-    } else {
-        errorf("unknown fail mode\n");
-    }
+    const bool print_tests = !ctrl.full_run && (ctrl.work_max - ctrl.work_offset) <= 10;
 
-    fail_count = strtoul(arg_fail_count, NULL, 10);
-
-    {
-        int d;
-        int k;
-        int ec = sscanf(arg_ecc_conf, "%i/%i", &d, &k);
-        if (ec != 2) {
-            errorf("failed to read ecc conf\n");
-        }
-        if (strcmp(arg_ecc_method, "hamming") == 0) {
-            method = new ECCMethod_Hamming();
-        } else if (strcmp(arg_ecc_method, "bch") == 0) {
-            method = new ECCMethod_BCH(d, k);
-        } else if (strcmp(arg_ecc_method, "hsiao") == 0) {
-            method = new ECCMethod_Hsiao(d, k, debug_print);
-        } else {
-            errorf("unknown ecc method\n");
-        }
-    }
-
-    const uint32_t data_with = method->DataWidth();
-    const uint32_t ecc_width = method->ECCWidth();
-    const uint32_t word_width = data_with + ecc_width;
-
-    full_run = strcmp(arg_test_count, "F") == 0;
-    if (full_run) {
-        test_count = nCr(word_width, fail_count);
-    } else {
-        test_count = strtoul(arg_test_count, NULL, 10);
-    }
-
-    srand(time(NULL)); // quick and dirty randomness if no seed given
-    seed = arg_seed == NULL ? rand() : strtoull(arg_seed, NULL, 10);
-
-    // program begins
-    ///////
-
-    assert(fail_count <= 8);
-
-    uint64_t rctr = 0; // random ctr
-
-    const bool print_tests = !full_run && test_count <= 10;
-
-    struct ecc_stats {
-        uint64_t detection_ok;
-        uint64_t detection_corrected;
-        uint64_t detection_uncorrectable;
-        uint64_t false_corrections;
-    } stats = (ecc_stats){
-        .detection_ok = 0,
-        .detection_corrected = 0,
-        .detection_uncorrectable = 0,
-        .false_corrections = 0,
-    };
-
-    printf("datawidth: %u ; eccwidth: %u\n", data_with, ecc_width);
-
-    if (full_run) {
-        char testcount_str[SPACED_U64_MAX_STR_SIZE];
-        pre_format_spaced_u64(testcount_str, test_count, ' ');
-        printf("full run: %s tests\n", testcount_str);
-    }
+    uint32_t data_width = ctrl.method->DataWidth();
+    uint32_t ecc_width = ctrl.method->ECCWidth();
+    uint32_t word_width = data_width + ecc_width;
 
     std::vector<bool> data;
-    data.resize(data_with);
+    data.resize(data_width);
     std::vector<bool> ecc;
     ecc.resize(ecc_width);
 
@@ -212,42 +151,41 @@ int main(int argc, char** argv)
 
     // randomize initial data
     for (uint32_t i = 0; i < data.size(); i++) {
-        data[i] = squirrelnoise5_u64(rctr++, seed) & 0b1;
+        data[i] = squirrelnoise5_u64(rctr++, ctrl.rng_seed) & 0b1;
     }
     // zero ecc
     for (uint32_t i = 0; i < data.size(); i++) {
         ecc[i] = 0;
     }
 
-    for (uint64_t t = 0; t < test_count; t++) {
+    for (uint64_t t = 0; ctrl.work_offset + t < ctrl.work_max; t++) {
         if (print_tests) {
             printf("\n\n");
         } else if ((t & UINT16_MAX) == 0) {
-            printf("\rprogress: %.5f", (float)t / (float)test_count);
-            fflush(stdout);
+            ctrl.work_progress = t;
         }
         // rebuild ecc
-        method->ConstructECC(data, ecc);
+        ctrl.method->ConstructECC(data, ecc);
         data_check = data;
         ecc_check = ecc;
         // inject bit faults
-        uint32_t fail_positions[fail_count];
+        uint32_t fail_positions[ctrl.fail_count];
         uint32_t total_positions = word_width;
         uint32_t generated_bits = 0;
 
-        switch (fail_mode) {
+        switch (ctrl.fail_mode) {
             case FAIL_MODE_NONE: {
                 //pass
             } break;
             case FAIL_MODE_RANDOM: {
-                if (full_run) {
-                    std::array<uint16_t, 8> bit_positions = bit_position_enumeration_idx_ncr(word_width, fail_count, t);
-                    for (; generated_bits < fail_count; generated_bits++) {
+                if (ctrl.full_run) {
+                    std::array<uint16_t, 8> bit_positions = bit_position_enumeration_idx_ncr(word_width, ctrl.fail_count, t);
+                    for (; generated_bits < ctrl.fail_count; generated_bits++) {
                         fail_positions[generated_bits] = bit_positions[generated_bits];
                     }
                 } else {
-                    while (generated_bits < fail_count) {
-                        uint32_t flip_pos = noise_get_u64n(rctr++, seed, total_positions);
+                    while (generated_bits < ctrl.fail_count) {
+                        uint32_t flip_pos = noise_get_u64n(rctr++, ctrl.rng_seed, total_positions);
                         bool unique = true;
                         for (uint32_t test_bit = 0; test_bit < generated_bits; test_bit++) {
                             if (fail_positions[test_bit] == flip_pos) {
@@ -262,15 +200,15 @@ int main(int argc, char** argv)
                 }
             } break;
             case FAIL_MODE_RANDOM_BURST: {
-                if (full_run) {
-                    std::array<uint16_t, 8> bit_positions = bit_position_enumeration_idx_burst(word_width, fail_count, t);
-                    for (; generated_bits < fail_count; generated_bits++) {
+                if (ctrl.full_run) {
+                    std::array<uint16_t, 8> bit_positions = bit_position_enumeration_idx_burst(word_width, ctrl.fail_count, t);
+                    for (; generated_bits < ctrl.fail_count; generated_bits++) {
                         fail_positions[generated_bits] = bit_positions[generated_bits];
                     }
                 } else {
-                    total_positions -= fail_count - 1;
-                    uint32_t flip_pos = noise_get_u64n(rctr++, seed, total_positions);
-                    while (generated_bits < fail_count) {
+                    total_positions -= ctrl.fail_count - 1;
+                    uint32_t flip_pos = noise_get_u64n(rctr++, ctrl.rng_seed, total_positions);
+                    while (generated_bits < ctrl.fail_count) {
                         fail_positions[generated_bits] = flip_pos + generated_bits;
                         generated_bits++;
                     }
@@ -350,7 +288,7 @@ int main(int argc, char** argv)
         }
 
         // check and correct
-        ECC_DETECTION detection = method->CheckAndCorrect(data, ecc);
+        ECC_DETECTION detection = ctrl.method->CheckAndCorrect(data, ecc);
 
         // print correction flips if wanted
         for (uint32_t bit_pos = 0; bit_pos < (data_fault.size() + ecc_fault.size()); bit_pos++) {
@@ -388,35 +326,35 @@ int main(int argc, char** argv)
         // print detection result
         switch (detection) {
             case ECC_DETECTION_OK: {
-                stats.detection_ok++;
+                ctrl.stats.detection_ok++;
                 if (print_tests) {
                     printf("detection: ok\n");
-                    if (fail_mode != FAIL_MODE_NONE && fail_count > 0) {
+                    if (ctrl.fail_mode != FAIL_MODE_NONE && ctrl.fail_count > 0) {
                         printf("completely silent corruption\n");
                     }
                 }
             } break;
             case ECC_DETECTION_CORRECTED: {
-                stats.detection_corrected++;
+                ctrl.stats.detection_corrected++;
                 if (print_tests) {
                     printf("detection: corrected\n");
                 }
                 bool correct_correction = true;
-                for (uint32_t i = 0; i < data_with; i++) {
+                for (uint32_t i = 0; i < data_width; i++) {
                     correct_correction &= data_check[i] == data[i];
                 }
                 for (uint32_t i = 0; i < ecc_width; i++) {
                     correct_correction &= ecc_check[i] == ecc[i];
                 }
                 if (!correct_correction) {
-                    stats.false_corrections++;
+                    ctrl.stats.false_corrections++;
                     if (print_tests) {
                         printf("correction failed\n");
                     }
                 }
             } break;
             case ECC_DETECTION_UNCORRECTABLE: {
-                stats.detection_uncorrectable++;
+                ctrl.stats.detection_uncorrectable++;
                 if (print_tests) {
                     printf("detection: uncorrectable\n");
                 }
@@ -429,10 +367,153 @@ int main(int argc, char** argv)
         }
     }
 
+    ctrl.work_progress = ctrl.work_max - ctrl.work_offset;
+
+    pthread_exit(NULL);
+}
+
+int main(int argc, char** argv)
+{
+    bool full_run = false;
+    FAIL_MODE fail_mode;
+    uint32_t fail_count;
+    uint64_t test_count;
+    uint64_t seed = 42;
+
+    // parse clas
+    if (argc < 7) {
+        errorf("usage: <threads> <fail_mode> <fail_count> <test_count> <ecc_method> <ecc_conf> [seed]\n");
+    }
+
+    const char* arg_thread_count = argv[1];
+    const char* arg_fail_mode = argv[2];
+    const char* arg_fail_count = argv[3];
+    const char* arg_test_count = argv[4];
+    const char* arg_ecc_method = argv[5];
+    const char* arg_ecc_conf = argv[6];
+    const char* arg_seed = argc > 7 ? argv[7] : NULL;
+    bool debug_print = argc > 8;
+
+    int thread_count = strtoul(arg_thread_count, NULL, 10);
+    if (thread_count == 0) {
+        thread_count = 1;
+    } else if (thread_count > std::thread::hardware_concurrency()) {
+        thread_count = std::thread::hardware_concurrency();
+    }
+
+    std::vector<thread_control> threads(thread_count);
+
+    if (strcmp(arg_fail_mode, "N") == 0) {
+        fail_mode = FAIL_MODE_NONE;
+    } else if (strcmp(arg_fail_mode, "R") == 0) {
+        fail_mode = FAIL_MODE_RANDOM;
+    } else if (strcmp(arg_fail_mode, "RB") == 0) {
+        fail_mode = FAIL_MODE_RANDOM_BURST;
+    } else {
+        errorf("unknown fail mode\n");
+    }
+
+    fail_count = strtoul(arg_fail_count, NULL, 10);
+    assert(fail_count <= 8);
+
+    {
+        int d;
+        int k;
+        int ec = sscanf(arg_ecc_conf, "%i/%i", &d, &k);
+        if (ec != 2) {
+            errorf("failed to read ecc conf\n");
+        }
+        if (strcmp(arg_ecc_method, "hamming") == 0) {
+            for (int tid = 0; tid < threads.size(); tid++) {
+                threads[tid].method = new ECCMethod_Hamming();
+            }
+        } else if (strcmp(arg_ecc_method, "bch") == 0) {
+            for (int tid = 0; tid < threads.size(); tid++) {
+                threads[tid].method = new ECCMethod_BCH(d, k);
+            }
+        } else if (strcmp(arg_ecc_method, "hsiao") == 0) {
+            for (int tid = 0; tid < threads.size(); tid++) {
+                threads[tid].method = new ECCMethod_Hsiao(d, k, debug_print);
+            }
+        } else {
+            errorf("unknown ecc method\n");
+        }
+    }
+
+    const uint32_t data_width = threads[0].method->DataWidth();
+    const uint32_t ecc_width = threads[0].method->ECCWidth();
+    const uint32_t word_width = data_width + ecc_width;
+
+    full_run = strcmp(arg_test_count, "F") == 0;
+    if (full_run) {
+        test_count = nCr(word_width, fail_count);
+    } else {
+        test_count = strtoul(arg_test_count, NULL, 10);
+    }
+
+    srand(time(NULL)); // quick and dirty randomness if no seed given
+    seed = arg_seed == NULL ? rand() : strtoull(arg_seed, NULL, 10);
+    uint64_t rctr = 0;
+
+    const bool print_tests = !full_run && test_count <= 10;
+
+    uint64_t work_per_thread = test_count / thread_count;
+    uint64_t rest_work = test_count % thread_count;
+
+    // set final thread launching arguments
+    for (int tid = 0; tid < threads.size(); tid++) {
+        threads[tid].full_run = full_run;
+        threads[tid].fail_mode = fail_mode;
+        threads[tid].fail_count = fail_count;
+        threads[tid].rng_seed = squirrelnoise5_u64(rctr++, seed);
+        threads[tid].work_offset = tid * work_per_thread;
+        threads[tid].work_max = (tid + 1) * work_per_thread + (tid == threads.size() - 1 ? rest_work : 0);
+    }
+
+    printf("datawidth: %u ; eccwidth: %u\n", data_width, ecc_width);
+    if (full_run) {
+        char testcount_str[SPACED_U64_MAX_STR_SIZE];
+        pre_format_spaced_u64(testcount_str, test_count, ' ');
+        printf("full run: %s tests\n", testcount_str);
+    }
+
+    // launch
+    for (int tid = 0; tid < threads.size(); tid++) {
+        pthread_create(&threads[tid].pthread_id, NULL, thread_work, &threads[tid]);
+    }
+
+    // report progress
+    while (true) {
+        uint64_t work_progress = 0;
+        for (int tid = 0; tid < threads.size(); tid++) {
+            work_progress += threads[tid].work_progress;
+        }
+        if (!print_tests) {
+            printf("\rprogress: %.5f", (float)work_progress / (float)test_count);
+            fflush(stdout);
+        }
+        if (work_progress == test_count) {
+            break;
+        }
+        usleep(150 * 1000); // 150ms
+    }
+
+    ecc_stats stats;
+
+    // collect
+    for (int tid = 0; tid < threads.size(); tid++) {
+        pthread_join(threads[tid].pthread_id, NULL);
+        stats.detection_ok += threads[tid].stats.detection_ok;
+        stats.detection_corrected += threads[tid].stats.detection_corrected;
+        stats.detection_uncorrectable += threads[tid].stats.detection_uncorrectable;
+        stats.false_corrections += threads[tid].stats.false_corrections;
+    }
+
+    // report results
     if (print_tests) {
         printf("\n\n");
     } else {
-        printf("\rprogress: 1.00\n");
+        printf("\rprogress: 1.00\n\n");
     }
     printf("stats:\n");
     printf("detection ok%s: %lu\n", fail_count == 0 ? "" : " (sdcs)", stats.detection_ok);
@@ -441,4 +522,7 @@ int main(int argc, char** argv)
 
     printf("done\n");
     return 0;
+
+    // program begins
+    ///////
 }
